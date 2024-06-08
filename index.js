45f8bc960cd3e65667a8070dd9f0bafd5f046d2c
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-06-08 14:02:38 +0600

    users api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,18 +31,62 @@ async function run() {
 
 
     const upazilaCollections = client.db('medidxDB').collection("upazila");
-    const districtsCollections = client.db('medidxDB').collection("districts");
+    const districtsCollection = client.db('medidxDB').collection("districts");
+    const usersCollection = client.db('medidxDB').collection("users");
 
     //all upazila get api 
     app.get('/upazila', async(req,res)=>{
         const result = await upazilaCollections.find().toArray();
         res.json(result);
     })
-
+    //all districts get api
    app.get('/districts', async(req,res)=>{
-    const result = await districtsCollections.find().toArray();
+    const result = await districtsCollection.find().toArray();
     res.json(result);
    })
+
+   //all users post api
+   app.post('/users', async(req,res)=> {
+    const {email} = req.query;
+    // console.log(email);
+    const existsUser = await usersCollection.findOne({email:email});
+    
+    if(!existsUser){
+      const user = req.body;
+       const result = await usersCollection.insertOne(user);
+       res.json(result);
+    }
+    if(existsUser){res.status(400).json({message:"user already exists"})}
+ 
+   })
+
+   //specific user by email
+   app.get('/users', async(req,res)=>{
+    const {email} = req.query;
+    const result = await usersCollection.findOne({email});
+    res.json(result);
+   })
+
+   //update a user information api
+   app.patch('/users/:id', async(req,res)=>{
+    const id = req.params.id;
+    const user = req.body;
+    // console.log(id,user);
+    const updateUser = {
+      $set:{
+        name: user?.name,
+          imageURL: user?.imageURL,
+          bloodGroup: user?.bloodGroup,
+          district: user?.district,
+          upazila: user?.upazila,
+      }
+    }
+    const query = {_id: new ObjectId(id)};
+    const result = await usersCollection.updateOne(query,updateUser);
+    res.json(result);
+   })
+
+
 
 
 
