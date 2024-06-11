8bc402c7f2bb20a3a8ce312729a62ac99264cb2c
Jahid Hasan Rony <jahidrony162@gmail.com>
2024-06-11 22:33:45 +0600

    two admin realted api all users and specifec user and one user status api added

--- index.js
+++ index.js
@@ -10,27 +10,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//custom middleware
 
-const verifyToken = async (req,res,next)=>{
-  // console.log('inside verify token', req.headers);
-  if(!req.headers?.authorization){
-    return res.status(401).json({message:"UnAuthorized Access"});
-  }
-  if(req.headers?.authorization){
-    const token = (req.headers?.authorization).split(' ')[1];
-    if(!token){
-      return res.status(403).json({message: "Forbidden Access"})
-    }
-    jwt.verify(token,process.env.SECRET_ACCESS_TOKEN, (err,decoded)=>{
-      if(err){
-        return res.status(401).json({message:'Forbidden Access'})
-      }
-      req.decoded=decoded;
-          next();
-    })
-  }
-}
+
+
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.k7qynmg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster`;
@@ -65,6 +49,57 @@ async function run() {
 
     })
 
+
+    //custom middleware
+
+//verify token
+const verifyToken =  (req,res,next)=>{
+  // console.log('inside verify token', req.headers);
+  if(!req.headers?.authorization){
+    return res.status(401).json({message:"UnAuthorized Access"});
+  }
+  if(req.headers?.authorization){
+    const token = (req.headers?.authorization).split(' ')[1];
+    if(!token){
+      return res.status(403).json({message: "Forbidden Access"})
+    }
+     jwt.verify(token,process.env.SECRET_ACCESS_TOKEN, (err,decoded)=>{
+      if(err){
+        return res.status(401).json({message:'Forbidden Access'})
+      }
+      // console.log(decoded);
+      req.decoded=decoded;
+          next();
+    })
+  }
+}
+
+    //verify admin
+    const verifyAdmin = async (req,res,next)=>{
+      const email = req.decoded?.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      if(user?.role!=='admin'){
+        return res.status(403).json({message:'unauthorize access'});
+      }
+      if(user?.role==='admin'){
+        next();
+      }
+    }
+
+    //check a user admin or not
+    app.get('/users/admin',verifyToken, async(req,res)=> {
+      const email = req.query.email;
+      const query = {email:email};
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if(user){
+        user?.role ==='admin'?admin=true:admin=false;
+      }
+      return res.json(admin)
+
+    })
+
     //all upazila get api 
     app.get('/upazila', async(req,res)=>{
         const result = await upazilaCollections.find().toArray();
@@ -81,13 +116,14 @@ async function run() {
     const {email} = req.query;
     // console.log(email);
     const existsUser = await usersCollection.findOne({email:email});
-    
+    console.log(existsUser);
+    if(existsUser){return res.json({message:"user already exists"})}
     if(!existsUser){
       const user = req.body;
        const result = await usersCollection.insertOne(user);
        res.json(result);
     }
-    if(existsUser){res.status(400).json({message:"user already exists"})}
+    
  
    })
 
@@ -98,6 +134,15 @@ async function run() {
     const result = await usersCollection.findOne({email});
     res.json(result);
    })
+  
+   //specific user by userID for admin
+   app.get('/users/:id',verifyToken,verifyAdmin, async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.findOne(query);
+      res.json(result);
+   })
+
 
    //update a user information api
    app.patch('/users/:id',verifyToken, async(req,res)=>{
@@ -116,6 +161,30 @@ async function run() {
     }
     const query = {_id: new ObjectId(id)};
     const result = await usersCollection.updateOne(query,updateUser);
+    res.json(result);
+   })
+
+   //get all users data by admin only
+   app.get('/all-users',verifyToken,verifyAdmin, async(req,res)=> {
+    const result = await usersCollection.find().toArray();
+    res.json(result);
+   })
+
+   //update user status api
+   app.patch('/users/status/:id',verifyToken,verifyAdmin, async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const userStatus = req.body;
+    const updateStatus={
+      $set:{
+        status: userStatus.status
+      }
+    }
+
+    
+
+    // console.log(updateRole);
+    const result = await usersCollection.updateOne(query,updateStatus);
     res.json(result);
    })
 
